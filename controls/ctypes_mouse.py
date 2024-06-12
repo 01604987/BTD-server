725d6c30af0f681b776fcf2352ce8f3944101a8f
01604987 <e1604987@student.tuwien.ac.at>
2024-06-12 23:55:25 +0200

    added support for mac

--- controls/ctypes_mouse.py
+++ controls/ctypes_mouse.py
@@ -1,23 +1,55 @@
-import ctypes
+import platform
+
+system = platform.system()
+
+print(f"Using {system} specific functions")
+
+if system == 'Windows':
 
 
-# Load user32.dll for Windows API calls
-user32 = ctypes.windll.user32
+    import ctypes
 
 
-MOUSEEVENTF_LEFTDOWN = 0x0002
-MOUSEEVENTF_LEFTUP = 0x0004
+    # Load user32.dll for Windows API calls
+    user32 = ctypes.windll.user32
 
-# Function to move the mouse to (x, y)
-def move_mouse(x, y):
-    user32.SetCursorPos(x, y)
 
-def move_mouse_relative(dx, dy):
-    user32.mouse_event(0x0001, ctypes.c_long(dx), ctypes.c_long(dy), 0, 0)
+    MOUSEEVENTF_LEFTDOWN = 0x0002
+    MOUSEEVENTF_LEFTUP = 0x0004
 
-def hold_lmb():
-    user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    # Function to move the mouse to (x, y)
+    def move_mouse(x, y):
+        user32.SetCursorPos(x, y)
 
-# Function to release the left mouse button
-def release_lmb():
-    user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+    def move_mouse_relative(dx, dy):
+        user32.mouse_event(0x0001, ctypes.c_long(dx), ctypes.c_long(dy), 0, 0)
+
+    def hold_lmb():
+        user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+
+    # Function to release the left mouse button
+    def release_lmb():
+        user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+elif system == 'Darwin':
+
+    from pynput.mouse import Button, Controller
+
+    # Initialize the mouse controller
+    mouse = Controller()
+
+    def move_mouse(x, y):
+        mouse.position = (x, y)
+
+    def move_mouse_relative(dx, dy):
+        current_x, current_y = mouse.position
+        mouse.position = (current_x + dx, current_y + dy)
+
+    def hold_lmb():
+        mouse.press(Button.left)
+
+    def release_lmb():
+        mouse.release(Button.left)
+        
+else:
+    raise Exception("Unsupported Operating System")
